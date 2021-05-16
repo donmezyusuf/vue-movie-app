c1696b2136cc94a3d1985afabf1b609c39550c74
Yusuf Dönmez <yusuf.donmez@hey.com>
2021-05-16 03:13:27 +0300

    Searchbar, Routing, Homepage development

---
+++ src/views/Homepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Container">
+
+    {{this.message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Homepage",
+
+};
+</script>
+
+<style scoped>
+.Container{
+  margin: auto;
+  display: flex;
+  background-color: red;
+  min-width: 1200px;
+  width: 85vw;
+  min-height: 100vh;
+}
+
+</style>
